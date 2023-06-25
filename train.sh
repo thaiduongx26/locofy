86362ed853c890ca3cedbfe98651ac6d4027f475
Howard <howard@192.168.2.4>
2023-06-25 15:05:54 +0700

    update config for train

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 file_list=()
-num_epochs=5
+num_epochs=20
 
 for i in {0..4}; do
     filepath="Data/fold_${i}.json"
@@ -16,7 +16,7 @@ done
 for filepath in "${file_list[@]}"; do
     filename=$(basename "$filepath" | cut -d. -f1)
     echo "Executing training for $filename"
-    python trainer.py --num_epochs $num_epochs --ver_name $filename --do_train --do_eval --do_save --output_dir "output2/"
+    python trainer.py --num_epochs $num_epochs --ver_name $filename --do_train --do_eval --do_save
     echo "Finished executing training: $filename"
 done
 
@@ -24,8 +24,7 @@ done
 filepath="Data/all.json"
 echo "Executing training for all.json data"
 filename="all"
-python trainer.py --num_epochs $num_epochs --ver_name $filename --do_train --do_save --output_dir "output2/"
+python trainer.py --num_epochs $num_epochs --ver_name $filename --do_train --do_save
 echo "Finished executing training: $filename"
 
 echo "Finished executing all training"
-
